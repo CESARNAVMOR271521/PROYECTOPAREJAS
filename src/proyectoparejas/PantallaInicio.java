6cb2c20696a9f347a868904442ac4514fbcfb699
Cesar <namc090227hmcvrsa1@soycecytem.mx>
2025-12-03 08:52:51 -0600

    modifique PantallaInicio

--- src/proyectoparejas/PantallaInicio.java
+++ src/proyectoparejas/PantallaInicio.java
@@ -5,6 +5,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import java.awt.FlowLayout;
 import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
 
 public class PantallaInicio {
 
@@ -44,7 +46,13 @@ public class PantallaInicio {
 		frmPrimeraPrueba.getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("HOLA MUNDO");
-		lblNewLabel.setBounds(77, 82, 264, 75);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 25));
+		lblNewLabel.setBounds(31, 74, 184, 75);
 		frmPrimeraPrueba.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("FILLO DA PUTA");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 25));
+		lblNewLabel_1.setBounds(41, 145, 248, 63);
+		frmPrimeraPrueba.getContentPane().add(lblNewLabel_1);
 	}
 }
